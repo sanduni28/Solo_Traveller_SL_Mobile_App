1956f390ba999d1714f00d185a84cb1720615ad2
sanduni28 <s22010154@ousl.lk>
2025-03-01 17:10:38 +0530

    changes

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+fun buildFeatures(function: () -> Boolean) {
+    TODO("Not yet implemented")
+}
+
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.s22010154.mysolotravelerslmobileapplication"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.s22010154.mysolotravelerslmobileapplication"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
+    implementation("com.android.car.ui:car-ui-lib:2.6.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.google.android.material:material:1.4.0")
+}
